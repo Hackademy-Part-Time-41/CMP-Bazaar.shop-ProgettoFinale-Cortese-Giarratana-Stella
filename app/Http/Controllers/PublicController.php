08c440b4f9f192331db2ab1d97b201df83648554
Matteo Giarratana <matteosmile@live.it>
2024-11-13 20:59:40 +0100

    Search migliorata

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -7,14 +7,25 @@ use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
-    public function homepage() {
-        $articles = Article::where('is_accepted',true)->take(6)->orderBy('created_at','desc')->get();
-        return view ('welcome',compact('articles'));
+    public function homepage()
+    {
+        $articles = Article::where('is_accepted', true)->take(6)->orderBy('created_at', 'desc')->get();
+        return view('welcome', compact('articles'));
     }
 
-    public function searchArticles(Request $request){
-        $query=$request->input('query');
-        $articles=Article::search($query)->where('is_accepted',true)->paginate(10);
-        return view('article.searched',['articles'=>$articles, 'query'=>$query]);
+    public function searchArticles(Request $request)
+    {
+        $query = $request->input('query');
+
+        if ($query == '') {
+
+            $articles = Article::where('is_accepted', true)->paginate(10);
+
+        } else {
+
+            $articles = Article::search($query)->where('is_accepted', true)->paginate(10);
+        }
+        
+        return view('article.searched', ['articles' => $articles, 'query' => $query]);
     }
 }

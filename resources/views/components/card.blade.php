8da850335ad420ffa5bff8c93f682498879018e5
Matteo Giarratana <matteosmile@live.it>
2024-11-13 21:28:29 +0100

    Card upgrade

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,7 @@
 <div class="card mx-auto card-w shadow text-center mb-3">
     <img src="https://picsum.photos/200" alt="immagine dell'articolo {{$article->title}}" class="card-img-top">
     <div class="card-body">
-        <h4 class="card-title">{{$article->title}}</h4>
+        <h4 class="card-title contenitore-fisso">{{$article->title}}</h4>
         <h6 class="card-subtitle text-body-secondary">{{$article->price}}€</h6>
         <div class="d-flex justify-content-evenly align-items-center mt-5">
             <a href="{{ route ('article.show', compact('article')) }}" class="btn btn-primary">Dettaglio</a>

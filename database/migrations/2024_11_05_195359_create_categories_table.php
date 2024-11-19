43a81f8456b2f30ee214ec358588e5e228b14f1e
Chiara Cortese <chiaracort99@gmail.com>
2024-11-19 20:27:18 +0100

    modifica categorie

--- database/migrations/2024_11_05_195359_create_categories_table.php
+++ database/migrations/2024_11_05_195359_create_categories_table.php
@@ -11,11 +11,30 @@ return new class extends Migration
      */
     public function up(): void
     {
+        $categories = [
+            'elettronica',
+            'abbigliamento',
+            'salute e bellezza',
+            'casa e giardinaggio',
+            'giocattoli',
+            'sport',
+            'animali domestici',
+            'libri e riviste',
+            'accessori',
+            'motori'
+        ];
+
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
         });
+
+        foreach ($categories as $category) {
+            Category::create([
+                'name'=> $category
+            ]);
+        }
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,27 +11,12 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
 
-    public $categories = [
-        'elettronica',
-        'abbigliamento',
-        'salute e bellezza',
-        'casa e giardinaggio',
-        'giocattoli',
-        'sport',
-        'animali domestici',
-        'libri e riviste',
-        'accessori',
-        'motori'
-    ];
+    
 
     public function run(): void
     {
 
-        // foreach ($this->categories as $category) {
-        //     Category::create([
-        //         'name'=> $category
-        //     ]);
-        // }
+       
 
         User::factory(10)->create();
         Article::factory(20)->create();

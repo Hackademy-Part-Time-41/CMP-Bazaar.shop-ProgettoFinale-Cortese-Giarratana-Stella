0206789a97f098ecad73710e8e4c64bb44fa1c35
riumudutraz <riumudu@posteo.com>
2024-11-19 21:14:36 +0100

    perfezionata migrazione categorie

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
+
+    protected $fillable = ['name'];
+
     public function articles ():HasMany
     {
         return $this->hasMany(Article::class);

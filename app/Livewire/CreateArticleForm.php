c6cdaa56e0e141e7894fabc55647531bc476999e
Matteo Giarratana <matteosmile@live.it>
2024-11-20 20:28:14 +0100

    user story-5

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -21,11 +21,12 @@ class CreateArticleForm extends Component
     #[Validate('required')]
     public $category;
     public $article;
-    
-    public $images=[];
+
+    public $images = [];
     public $temporary_images;
 
-    public function store() {
+    public function store()
+    {
         $this->validate();
         $this->article = Article::create([
             'title' => $this->title,
@@ -35,6 +36,11 @@ class CreateArticleForm extends Component
             'user_id' => Auth::id()
         ]);
 
+        if (count($this->images) > 0) {
+            foreach ($this->images as $image) {
+                $this->article->images()->create(['path' => $image->store('images', 'public')]);
+            }
+        }
         // $this->reset();
 
         // session()->flash('success', 'Articolo creato correttamente');
@@ -42,8 +48,26 @@ class CreateArticleForm extends Component
         return redirect()->route('article.draft', $this->article)->with('success', 'Articolo in attesa di revisione');
     }
 
+    public function updatedTemporaryImages() {
+        if($this->validate([
+            'temporary_images.*' => 'image|max:1024',
+            'temporary_images' => 'max:6'
+        ])){
+            foreach ($this->temporary_images as $image) {
+                $this->images[] = $image;
+            }
+        };
+    }
 
-    public function render(){
+    public function removeImage($key) {
+        if (in_array($key, array_keys($this->images))) {
+            unset($this->images[$key]);
+        }
+    }
+
+
+    public function render()
+    {
         return view('livewire.create-article-form');
     }
 }

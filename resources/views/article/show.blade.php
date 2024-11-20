57541556da5500572600db07c32f83e2e701d519
Matteo Giarratana <matteosmile@live.it>
2024-11-20 21:00:05 +0100

    Soluzione immagini nella show

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -11,10 +11,11 @@
         <div class="row height-custom justify-content-center py-5">
             <div class="col-12 col-md-6 mb-3">
                 @if ($article->images->count() > 0)
+                    {{-- @dd($article->images) --}}
                     <div id="carouselExample" class="carousel slide">
                         <div class="carousel-inner">
                             @foreach ($article->images as $key => $image)
-                                <div class="carousel-item" @if ($loop->first) active @endif>
+                                <div class="carousel-item @if ($loop->first) active @endif">
                                     <img src="{{ Storage::url($image->path) }}"
                                         alt="immagine {{ $key + 1 }} dell'articolo {{ $article->title }}"
                                         class="d-block w-100 rounded shadow">
@@ -38,16 +39,14 @@
                         <img src="https://picsum.photos/300" alt="nessuna foto inserita dall'utente">
                 @endif
             </div>
-        </div>
-        <div class="col-12 col-md-6 mb-3 height-custom text-center">
-            <h2 class="display-5"><span class="fw-bold">Titolo:</span>{{ $article->title }}</h2>
-            <div class="d-flex flex-column justify-content-center h-75">
-                <h4 class="fw-bold">Prezzo: {{ $article->price }}€</h4>
-                <h5>Descrizione:</h5>
-                <p>{{ $article->description }}</p>
+            <div class="col-12 col-md-6 mb-3 height-custom text-center">
+                <h2 class="display-5"><span class="fw-bold">Titolo:</span>{{ $article->title }}</h2>
+                <div class="d-flex flex-column justify-content-center h-75">
+                    <h4 class="fw-bold">Prezzo: {{ $article->price }}€</h4>
+                    <h5>Descrizione:</h5>
+                    <p>{{ $article->description }}</p>
+                </div>
             </div>
         </div>
     </div>
-    </div>
-
 </x-layout>

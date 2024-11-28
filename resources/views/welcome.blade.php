c843df4eaedb63697cf776caf3dd7f2c3c36ad07
riumudutraz <riumudu@posteo.com>
2024-11-28 19:44:29 +0100

    immagine bazaarshop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,32 +10,39 @@
     @endif
     <div style="height: 15vh"></div>
 
-    <div class="container-fluid text-center sfondo">
-        <div class="row vh-100 justify-content-center align-items-center">
-            <div class="col-12">
+    
+            {{-- <div class="col-12"> --}}
                 {{-- <h1 class="title-custom display-1">BazaarShop</h1> --}}
                 {{-- <div class="my-3">
                     @auth
                         <a href="{{ route('create.article') }}"><button type="button" class="btn btncustom">{{__('ui.createArticle')}}</button></a>
                     @endauth
                 </div> --}}
+            {{-- </div>
+        </div>
+    </div> --}}
+    <div class="container-fluid text-center">
+        <div class="row vh-80 justify-content-center align-items-center">
+            <div class="sfondo">
+                <img src="/img/wallpaper.png" alt="bazaarshop">
+            </div>
+            <div class="border border-2 border-danger rounded-4 my-1 col-10">
+            <p class="display-6">Guarda le ultime offerte</p>
+            {{-- card --}}
+            <div class="row justify-content-center my-5">
+            <div class="row height-custom justify-content-center align-items-center py-5 px-5 col-10">
+                @forelse ($articles as $article)
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <x-card :article="$article"/>
+                    </div>
+                @empty
+                    <div class="col-12">
+                        <h3 class="text-center">{{__('ui.noArticlesYet')}}</h3>
+                    </div>
+                @endforelse
             </div>
         </div>
     </div>
-
-    {{-- card --}}
-    <div class="row justify-content-center my-5">
-        <div class="row height-custom justify-content-center align-items-center py-5 px-5 col-10">
-            @forelse ($articles as $article)
-                <div class="col-12 col-md-6 col-lg-4">
-                    <x-card :article="$article"/>
-                </div>
-            @empty
-                <div class="col-12">
-                    <h3 class="text-center">{{__('ui.noArticlesYet')}}</h3>
-                </div>
-            @endforelse
-        </div>
     </div>
-
+    </div>
 </x-layout>
